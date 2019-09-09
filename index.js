41e88ca4acbd16bc61b4a166726bea386873e45a
Ballone <balloneij@ad.msoe.edu>
2019-09-09 18:04:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+
+console.log(process.env.SLACK_SIGNING_SECRET);
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// The echo command simply echoes on command
+app.command("/bonk", async ({ command, ack, say }) => {
+  // Acknowledge command request
+  ack();
+  console.log(`${command.text}`);
+  say(`${command.text}`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
