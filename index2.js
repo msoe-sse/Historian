fbb3d63eab61736dd80dd6f49341487b654935f5
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-10-10 21:49:23 -0500

    Adding back botkit example with slack adapter

---
+++ index2.js
@@ -0,0 +1,23 @@
+const Botkit = require('botkit');
+require('dotenv').config();
+const { SlackAdapter } = require('botbuilder-adapter-slack');
+
+const adapter = new SlackAdapter({
+    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+    botToken: process.env.BOT_TOKEN
+});
+
+const controller = Botkit({
+    adapter
+});
+
+controller.on('slash_command', function(bot, message) {
+    bot.replyAcknowledge();
+    switch(message.command) {
+        case '/bonk':
+            bot.reply(message, "bonk");
+            break;
+        default:
+            bot.reply(message, 'unknown command');
+    };
+});
