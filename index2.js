21120364b9fb647139ae5c529190d1ecbc32a930
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-10-10 22:08:04 -0500

    Trying to add some aditional code

--- index2.js
+++ index2.js
@@ -3,12 +3,23 @@ require('dotenv').config();
 const { SlackAdapter } = require('botbuilder-adapter-slack');
 
 const adapter = new SlackAdapter({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
     botToken: process.env.BOT_TOKEN
 });
 
 const controller = new Botkit({
     adapter: adapter
+});
+
+controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
+    if (err) process.exit(1);
+
+    webserver.use(function (req, res, next) {
+        next();
+    });
+    controller.createWebhookEndpoints(webserver);
 });
 
 controller.on('slash_command', function(bot, message) {
