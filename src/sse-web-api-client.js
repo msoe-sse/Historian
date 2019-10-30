fc8e38fab1724231ec76745c4de16846230904c2
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-10-29 20:45:19 -0500

    Adding functionality to call the SSE Web API to create a resource

---
+++ src/sse-web-api-client.js
@@ -0,0 +1,31 @@
+const config = require('../config/config.js');
+
+function createSSEResource(author, contents) {
+    return new Promise(function(resolve, reject) {
+        fetch(`${global.gConfig.apiBaseUrl}/resources`, {
+            method: 'post',
+            body: JSON.stringify({
+                author: author,
+                contents: contents
+            })
+        })
+        .then(res => res.json())
+        .then(function(data) {
+            let unexpectedResponse = false;
+
+            if(data === undefined || data.author === undefined || data.author !== author)
+                unexpectedResponse = true;
+
+            if(data === undefined || data.contents === undefined || data.contents !== contents)
+                unexpectedResponse = true;
+            
+            if(unexpectedResponse)
+                reject('Error: Unexpected Response from the SSE Web API');
+            else
+                resolve('Resource Successfully Saved.');
+        })
+        .catch(function(err) {
+            reject(`Error: ${err}`);
+        });
+    });
+}
